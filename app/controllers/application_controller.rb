48be985118e88a4e866b7853f6a8c3c5ff8705c1
Fatos Morina <fatosi.morina@gmail.com>
2017-05-12 14:31:21 +0200

    Delete the second mentioning of avatar
    Delete the second mentioning of avatar at added_attrs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   #configure permitted parameters for devise
   def configure_permitted_parameters
-    added_attrs = [:email, :password, :password_confirmation, :remember_me, :avatar, :avatar, :avatar_cache]
+    added_attrs = [:email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
